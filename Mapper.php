d6acb5ac6fdeb609b38cb2a188c9cdb1cea0b9d8
Anthony Gonzalez <darkdevilishag@gmail.com>
2015-09-08 17:11:47 -0400

    Adds update()

--- Mapper.php
+++ Mapper.php
@@ -14,7 +14,7 @@ abstract class Mapper extends DatabaseObject{
     $db = MySQLDatabase::getInstance();
     
     $attrs = $this->attributes();
-    unset($attrs['id']);
+    unset($attrs['id']);//no need to insert id mysql does it automatically
     $sql = $this->prepared_sql();
     $stmt = $db->prepared_stmt($sql);
     $params_type = static::check_bind_params_type($attrs);
@@ -25,6 +25,27 @@ abstract class Mapper extends DatabaseObject{
     $stmt->free_result();
     $stmt->close();
     return true;
+  }
+
+  function update() {
+    $db = MySQLDatabase::getInstance();
+    
+    $attrs = $this->attributes();
+    $attrs_pairs = array();
+    foreach($attrs as $key => $value){
+      $attrs_pairs[] = "{$key}='{$value}'";
+    }
+    $sql = "UPDATE ".static::$table_name." SET ";
+    $sql .= join(", ", $attrs_pairs);
+    $sql .= " WHERE id=". "?";
+    $stmt = $db->prepared_stmt($sql);
+    $bind_result = $stmt->bind_param("i", $this->id);
+    $db->confirm_bind_result($bind_result, $stmt);
+    $db->execute($stmt);
+    $affected_rows = $stmt->affected_rows;
+    $stmt->free_result();
+    $stmt->close();
+    return ($affected_rows == 1) ? true : false;
   }
 
   protected function attributes() {
@@ -48,7 +69,7 @@ abstract class Mapper extends DatabaseObject{
   
   private function prepared_sql() {
     $attributes = $this->prepared_attrs();
-    unset($attributes['id']);
+    unset($attributes['id']);//no need to insert id mysql does it automatically
     $sql = "INSERT INTO ".static::$table_name." (";
     $sql .= join(", ", array_keys($attributes));
     $sql .= ") VALUES (";
